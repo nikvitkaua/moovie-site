60e7cfb0562f6694275a7e9d69706324d6952b79
Mykyta Paziuka <m.paziuka@tsquad.net>
2025-07-11 14:42:22 +0300

    add dynamic content to site

--- js/script.js
+++ js/script.js
@@ -1,26 +1,36 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
-'use strict';
+"use strict";
 
 const movieDB = {
     movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+        "logan",
+        "injustice League",
+        "la-la-land",
+        "durability",
+        "scott pilgrim vs. the world",
+    ],
 };
 
+const adv = document.querySelectorAll(".promo__adv img"),
+      promoGenre = document.querySelector(".promo__genre"),
+      promoBg = document.querySelector(".promo__bg"),
+      watchedFilms = document.querySelector(".promo__interactive-list");
+
+adv.forEach((item) => {
+    item.remove();
+});
+
+promoGenre.textContent = "drama";
+
+promoBg.style.backgroundImage = "url('img/bg.jpg')";
+
+watchedFilms.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((movieName, i) => {
+    watchedFilms.innerHTML += `
+        <li class="promo__interactive-item">#${i + 1} ${movieName}
+            <div class="delete"></div>
+        </li>
+    `;
+});
