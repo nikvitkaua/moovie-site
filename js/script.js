9e7d365f8257a94503c8775e7293390d24841441
Mykyta Paziuka <m.paziuka@tsquad.net>
2025-07-16 08:52:53 +0300

    add DOMContentDoaded listener to js

--- js/script.js
+++ js/script.js
@@ -1,81 +1,83 @@
 "use strict";
 
-const movieDB = {
-    movies: [
-        "logan",
-        "injustice League",
-        "la-la-land",
-        "durability",
-        "scott pilgrim vs. the world",
-    ],
-};
+document.addEventListener("DOMContentLoaded", () => {
+    const movieDB = {
+        movies: [
+            "logan",
+            "injustice League",
+            "la-la-land",
+            "durability",
+            "scott pilgrim vs. the world",
+        ],
+    };
 
-const adv = document.querySelectorAll(".promo__adv img"),
-    promoGenre = document.querySelector(".promo__genre"),
-    promoBg = document.querySelector(".promo__bg"),
-    watchedFilms = document.querySelector(".promo__interactive-list"),
-    addFilmForm = document.querySelector(".add"),
-    addFilmInput = addFilmForm.querySelector(".adding__input"),
-    addFilmFavorite = addFilmForm.querySelector("[data-favorite]");
+    const adv = document.querySelectorAll(".promo__adv img"),
+        promoGenre = document.querySelector(".promo__genre"),
+        promoBg = document.querySelector(".promo__bg"),
+        watchedFilms = document.querySelector(".promo__interactive-list"),
+        addFilmForm = document.querySelector(".add"),
+        addFilmInput = addFilmForm.querySelector(".adding__input"),
+        addFilmFavorite = addFilmForm.querySelector("[data-favorite]");
 
-const deleteAdv = (arr) => {
-    arr.forEach((item) => {
-        item.remove();
-    });
-}
+    const deleteAdv = (arr) => {
+        arr.forEach((item) => {
+            item.remove();
+        });
+    };
 
-const makeChanges = () => {
-    promoGenre.textContent = "drama";
+    const makeChanges = () => {
+        promoGenre.textContent = "drama";
 
-    promoBg.style.backgroundImage = "url('img/bg.jpg')";
-}
+        promoBg.style.backgroundImage = "url('img/bg.jpg')";
+    };
 
-const sortArr = (arr) => {
-    arr.sort();
-}
+    const sortArr = (arr) => {
+        arr.sort();
+    };
 
-addFilmForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+    addFilmForm.addEventListener("submit", (e) => {
+        e.preventDefault();
 
-    if (addFilmFavorite.checked) {
-        console.log('Add to favorite')
-    }
-    
-    if (addFilmInput.value) {
-        movieDB.movies.push(addFilmInput.value.toLowerCase());
-        addFilmInput.value = "";
-    }
-
-    showWatchedFilms(movieDB.movies, watchedFilms);
-});
-
-function showWatchedFilms(films, parent) {
-    parent.innerHTML = "";
-
-    sortArr(films);
-
-    films.forEach((movieName, i) => {
-        if (movieName.length > 20) {
-            movieName = movieName.slice(0, 21) + "...";
+        if (addFilmFavorite.checked) {
+            console.log("Add to favorite");
         }
 
-        parent.innerHTML += `
+        if (addFilmInput.value) {
+            movieDB.movies.push(addFilmInput.value.toLowerCase());
+            addFilmInput.value = "";
+        }
+
+        showWatchedFilms(movieDB.movies, watchedFilms);
+    });
+
+    function showWatchedFilms(films, parent) {
+        parent.innerHTML = "";
+
+        sortArr(films);
+
+        films.forEach((movieName, i) => {
+            if (movieName.length > 20) {
+                movieName = movieName.slice(0, 21) + "...";
+            }
+
+            parent.innerHTML += `
             <li class="promo__interactive-item">#${i + 1} ${movieName}
                 <div class="delete"></div>
             </li>
         `;
-    });
-
-    document.querySelectorAll(".delete").forEach((delBtn, i) => {
-        delBtn.addEventListener("click", () => {
-            films.splice(i, 1);
-            delBtn.parentElement.remove();
-
-            showWatchedFilms(films, parent);
         });
-    });
-}
 
-deleteAdv(adv);
-makeChanges();
-showWatchedFilms(movieDB.movies, watchedFilms);
+        document.querySelectorAll(".delete").forEach((delBtn, i) => {
+            delBtn.addEventListener("click", () => {
+                films.splice(i, 1);
+                delBtn.parentElement.remove();
+
+                showWatchedFilms(films, parent);
+            });
+        });
+    }
+
+    deleteAdv(adv);
+    makeChanges();
+    showWatchedFilms(movieDB.movies, watchedFilms);
+});
